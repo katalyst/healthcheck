50cc402634aaa30d32040693128569802c499507
Andy Williams <andy.williams@katalyst.com.au>
2022-01-28 14:48:01 +1030

    Update Monitored concern to delay saving defined tasks
    saving tasks in define_task is breaking circleci specs as we can't get a redis lock in time to save 6 defined tasks all at once

--- lib/katalyst/healthcheck/monitored.rb
+++ lib/katalyst/healthcheck/monitored.rb
@@ -11,29 +11,51 @@ module Katalyst
         # @param description [String] A description of the task's function
         # @param interval [Integer,ActiveSupport::Duration] Expected frequency that this task runs, e.g. 1.day
         def define_task(name, description, interval:)
-          task = Task.new(name: name, description: description, interval: interval)
-          task.save
+          defined_healthcheck_tasks[name] = Task.new(name: name, description: description, interval: interval)
         end
 
         # Mark a task as healthy
         # @param name [Symbol] The name of the task
         def healthy!(name)
-          Task.find!(name).healthy!
+          find_or_create_task(name).healthy!
         end
 
         # Mark a task as unhealthy
         # @param name [Symbol] The name of the task
         # @param error [String] Optional error message
         def unhealthy!(name, error = nil)
-          Task.find!(name).unhealthy!(error)
+          find_or_create_task(name).unhealthy!(error)
+        end
+
+        private
+
+        def find_or_create_task(name)
+          task = Task.find(name)
+          if task.nil?
+            task = defined_healthcheck_tasks[name]
+            task&.save
+          end
+          task
         end
       end
 
       class_methods do
         include HealthMethods
+
+        attr_accessor :healthcheck_task_definitions
+
+        # @return [Hash] Defined tasks keyed by name
+        def defined_healthcheck_tasks
+          self.healthcheck_task_definitions ||= {}
+        end
       end
 
       include HealthMethods
+
+      # @return [Hash] Defined tasks keyed by name
+      def defined_healthcheck_tasks
+        self.class.healthcheck_task_definitions ||= {}
+      end
     end
   end
 end
